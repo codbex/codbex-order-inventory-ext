c56e27c238f602ccdc45e0ab25a45f2572a919c3
MinaDoncheva <mina.doncheva@icloud.com>
2024-06-26 14:25:37 +0300

    fix orders paths and separate in/out of stock items

--- codbex-order-inventory-ext/generate/GoodsIssue/generate-goods-issue-controller.js
+++ codbex-order-inventory-ext/generate/GoodsIssue/generate-goods-issue-controller.js
@@ -15,43 +15,51 @@ app.controller('templateController', ['$scope', '$http', 'ViewParameters', 'mess
     const salesOrderItemsUrl = "/services/ts/codbex-order-inventory-ext/generate/GoodsIssue/api/GenerateGoodsIssueService.ts/salesOrderItemsData/" + params.id;
     $http.get(salesOrderItemsUrl)
         .then(function (response) {
-            $scope.SalesOrderItemsData = response.data;
+            $scope.SalesOrderItemsData = response.data.ItemsForIssue;
+            $scope.ItemsToRestock = response.data.ItemsToRestock;
         })
         .catch(function (error) {
             console.error("Error retrieving sales order items data:", error);
         });
 
     $scope.generateGoodsIssue = function () {
-        const goodsIssueUrl = "/services/ts/codbex-inventory/gen/api/GoodsIssues/GoodsIssueService.ts/";
+        const itemsForIssue = $scope.SalesOrderItemsData;
 
-        $http.post(goodsIssueUrl, $scope.SalesOrderData)
-            .then(function (response) {
-                $scope.GoodsIssue = response.data;
+        if (itemsForIssue.length > 0) {
+            const goodsIssueUrl = "/services/ts/codbex-inventory/gen/api/GoodsIssues/GoodsIssueService.ts/";
 
-                if ($scope.SalesOrderItemsData && $scope.SalesOrderItemsData.length > 0) {
-                    itemsForIssue.forEach(orderItem => {
-                        const goodsIssueItem = {
-                            "GoodsIssue": $scope.GoodsIssue.Id,
-                            "Product": orderItem.Product,
-                            "Quantity": orderItem.Quantity,
-                            "UoM": orderItem.UoM,
-                            "Price": orderItem.Price,
-                            "Net": orderItem.Net,
-                            "VAT": orderItem.VAT,
-                            "Gross": orderItem.Gross
-                        };
-                        const goodsIssueItemUrl = "/services/ts/codbex-inventory/gen/api/GoodsIssues/GoodsIssueItemService.ts/";
-                        $http.post(goodsIssueItemUrl, goodsIssueItem);
-                    });
-                }
-                console.log("GoodsIssue created successfully:", response.data);
-                $scope.closeDialog();
-            })
-            .catch(function (error) {
-                console.error("Error creating GoodsIssue:", error);
+            $http.post(goodsIssueUrl, $scope.SalesOrderData)
+                .then(function (response) {
+                    $scope.GoodsIssue = response.data;
 
-                $scope.closeDialog();
-            });
+                    if ($scope.SalesOrderItemsData && $scope.SalesOrderItemsData.length > 0) {
+                        itemsForIssue.forEach(orderItem => {
+                            const goodsIssueItem = {
+                                "GoodsIssue": $scope.GoodsIssue.Id,
+                                "Product": orderItem.Product,
+                                "Quantity": orderItem.Quantity,
+                                "UoM": orderItem.UoM,
+                                "Price": orderItem.Price,
+                                "Net": orderItem.Net,
+                                "VAT": orderItem.VAT,
+                                "Gross": orderItem.Gross
+                            };
+                            const goodsIssueItemUrl = "/services/ts/codbex-inventory/gen/api/GoodsIssues/GoodsIssueItemService.ts/";
+                            $http.post(goodsIssueItemUrl, goodsIssueItem);
+                        });
+                    }
+                    console.log("GoodsIssue created successfully:", response.data);
+                    $scope.closeDialog();
+                })
+                .catch(function (error) {
+                    console.error("Error creating GoodsIssue:", error);
+
+                    $scope.closeDialog();
+                });
+        } else {
+            console.log("No items to issue. GoodsIssue not created.");
+            $scope.closeDialog();
+        }
     };
 
     $scope.closeDialog = function () {
