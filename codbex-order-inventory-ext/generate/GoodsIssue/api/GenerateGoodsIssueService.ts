e740779b9272c093457dc67a4333a7722868283d
ThuF <jordan.p.pavlov@gmail.com>
2024-07-08 19:15:57 +0300

    Revert new paths for release with 0.1.0

--- codbex-order-inventory-ext/generate/GoodsIssue/api/GenerateGoodsIssueService.ts
+++ codbex-order-inventory-ext/generate/GoodsIssue/api/GenerateGoodsIssueService.ts
@@ -1,6 +1,6 @@
-import { SalesOrderRepository as SalesOrderDao } from "../../../../codbex-orders/gen/codbex-orders/dao/salesorder/SalesOrderRepository";
-import { SalesOrderItemRepository as SalesOrderItemDao } from "../../../../codbex-orders/gen/codbex-orders/dao/salesorder/SalesOrderItemRepository";
-import { CatalogueRepository as catalogueRepositoryDao } from "codbex-products/gen/codbex-products/dao/Catalogues/CatalogueRepository";
+import { SalesOrderRepository as SalesOrderDao } from "../../../../codbex-orders/gen/dao/salesorder/SalesOrderRepository";
+import { SalesOrderItemRepository as SalesOrderItemDao } from "../../../../codbex-orders/gen/dao/salesorder/SalesOrderItemRepository";
+import { CatalogueRepository as catalogueRepositoryDao } from "codbex-products/gen/dao/Catalogues/CatalogueRepository";
 
 import { Controller, Get } from "sdk/http";
 
